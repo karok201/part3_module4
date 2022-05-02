23c658794c3eb06c35f5b88e99197da4460f443a
Nikita Babichenko <babichenkona@gmail.com>
2022-05-02 12:56:25 +0000

    Hotfix

--- app.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-$app = new \Symfony\Component\Console\Application('Some application');
-
-$app->run();
